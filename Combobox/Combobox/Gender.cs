e401c1dd373f5d4df971409291eac33d9275f627
Hemavathianandan <36996181+Hemavathibabes@users.noreply.github.com>
2020-06-25 15:45:49 +0530

    inotify

---
+++ Combobox/Combobox/Gender.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization;
+
+namespace Combobox
+{
+   public class Gender:INotifyPropertyChanged
+    {
+
+        /*  private string male;
+          private string female;
+          private string sitem;
+         public string Male
+          {
+              get
+              {
+                  return male;
+                  onPropertyChanged("Male");
+              }
+              set
+              {
+                  if (male != value)
+                  {
+                      male = value;
+                  }
+              }
+          }
+          public string Female
+          {
+              get
+              {
+                  return female;
+                  onPropertyChanged("Female");
+
+              }
+              set
+              {
+                  if (female != value)
+                  {
+                      female = value;
+                  }
+              }
+          }*/
+        public string sitem { get; set; }
+         
+        private string gendervalue;
+        public string Gendervalue
+        {
+            get
+            {
+                return gendervalue;
+            }
+            set
+            {
+                if(gendervalue!=null)
+                {
+                    gendervalue = value;
+                    onPropertyChanged("Gendervalue");
+                }
+            }
+        }
+        private void onPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+    
+}
+}
---
+++ Combobox/Combobox/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Combobox
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page,INotifyPropertyChanged
+    {
+      
+       private List<Gender> gender =new List<Gender>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            gender.Add( new Gender { sitem = "Male" });
+           gender.Add(new Gender { sitem = "Female" });
+            combo.ItemsSource = gender;
+          //  this.DataContext = gender;
+
+        }
+      
+       
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // text = (sender as ComboBox).SelectedItem as string;
+            string text = ((sender as ComboBox).SelectedItem as ComboBoxItem).Content as string;
+        
+        }
+    }
+}
